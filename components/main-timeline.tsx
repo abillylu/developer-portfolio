e56f392ecff139f1ea8cb7b65edbd183e1d78252
abillylu <a.billy.lu@gmail.com>
2025-09-26 23:56:13 +0800

    add more information in the experience page

--- components/main-timeline.tsx
+++ components/main-timeline.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
 	Timeline,
 	TimelineDot,
@@ -10,18 +12,45 @@ import {
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
-import { ExternalLink } from "lucide-react";
+import { ExternalLink, FolderClosed, Lightbulb, Medal, Newspaper } from "lucide-react";
+import { Badge } from "./ui/badge";
+
+import technologies from "@/data/technologies.json";
+
+type Technologies = typeof technologies;
+
+type TechnologyKeys = keyof Technologies;
+
+import TechCarousel from "./tech-carousel";
 
 interface ContentTimeline {
 	logo: string;
 	school: string;
+	location: {
+		text: string;
+		url: string;
+	};
 	duration: {
 		start: string;
-		end: string;
+		end?: string | null;
 	};
-	link: string | null;
+	link?: string | null;
 	program: string;
+	level: string | null;
 	description: string;
+	projects: {
+		title: string;
+		logo: string;
+		application: string;
+		duration: {
+			start: string;
+			end: string;
+		};
+		link?: string | null;
+	}[];
+	header: string;
+	skills: string[];
+	technologies: string[];
 }
 
 type Position = "left" | "right" | null | undefined;
@@ -91,9 +120,121 @@ export const MainTimeline = ({
 										</CardTitle>
 									</CardHeader>
 									<CardContent>
-										{/* <p className="text-justify">{education.description}</p> */}
-										{/* <p className="text-justify w-120"> */}
-										<p>{education.description}</p>
+										{education.technologies.length > 0 && (
+											<>
+												<div className="flex">
+													<Badge variant={"secondary"} className="mb-2">
+														<Newspaper />
+														{education.header}
+													</Badge>
+												</div>
+												<p className="p-2 text-justify">
+													{education.description}
+												</p>
+												{education.level && (
+													<p className="p-2 text-justify">
+														<Badge variant={"default"} className="mb-2">
+															<Medal />
+															{education.level}
+														</Badge>
+													</p>
+												)}
+
+												{education.projects?.length > 0 && (
+													<>
+														<div className="flex">
+															<Badge
+																variant={"secondary"}
+																className="mb-2"
+															>
+																<FolderClosed />
+																Projects
+															</Badge>
+														</div>
+														{education.projects?.map(
+															(project, index) => {
+																return (
+																	<Card
+																		key={index}
+																		className="mb-2 overflow-hidden"
+																	>
+																		<CardContent className="p-2">
+																			<div className="flex items-center gap-2">
+																				<Image
+																					width={50}
+																					height={50}
+																					src={
+																						project.logo
+																					}
+																					alt={
+																						project.title
+																					}
+																				/>
+																				<p key={index}>
+																					{project.title}
+																				</p>
+																			</div>
+																			<p>
+																				{
+																					project.application
+																				}
+																			</p>
+																			<p>
+																				{
+																					project.duration
+																						.start
+																				}
+																				{project.duration
+																					.end === "Done"
+																					? ""
+																					: ` - ${project.duration.end}`}
+																			</p>
+																		</CardContent>
+																	</Card>
+																);
+															},
+														)}
+													</>
+												)}
+												<div className="flex">
+													<Badge variant={"secondary"} className="mb-2">
+														<Lightbulb />
+														Technologies Learned
+													</Badge>
+												</div>
+											</>
+										)}
+										{education.technologies.length > 5 ? (
+											<div
+												className={
+													education.technologies.length < 5
+														? "flex justify-center"
+														: ""
+												}
+											>
+												<TechCarousel techStack={education.technologies} />
+											</div>
+										) : (
+											<div className="gap-1 flex flex-items flex-wrap">
+												{education.technologies.map(
+													(technology, index: number) => {
+														const key: TechnologyKeys =
+															technology as TechnologyKeys;
+														return (
+															<div key={index}>
+																<Image
+																	height={35}
+																	width={35}
+																	className="w-[50] h-[50]"
+																	src={technologies[key].logo}
+																	alt={technologies[key].name}
+																/>
+															</div>
+														);
+													},
+												)}
+											</div>
+										)}
 									</CardContent>
 									<CardFooter></CardFooter>
 								</Card>
